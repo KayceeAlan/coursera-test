569dac3eb57f068c333f9288178aa5b40cfd8a6d
Kelechi Alan <68669988+KayceeAlan@users.noreply.github.com>
2020-08-17 11:57:58 +0100

    Delete SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-	var byeSpeaker = {};
-	byeSpeaker.speakWord = "Good Bye";
-	byeSpeaker.speak = function (name) {
-		console.log(byeSpeaker.speakWord + " " + name);
-	  }
-	  window.byeSpeaker = byeSpeaker;
-})(window);
